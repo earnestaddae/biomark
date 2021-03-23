1ee96fea1d68bdb9487ef3ebeeb6ff1543997b45
Ernest Addae <earnestaddae@gmail.com>
2021-03-23 20:24:48 +0800

    Refactor solution

--- app/services/payload_sync/payloads.rb
+++ app/services/payload_sync/payloads.rb
@@ -53,41 +53,34 @@ module PayloadSync
         remote_labs = PAYLOADS
         remote_labs.each do |remote_lab|
           patient = create_patient(remote_lab)
-          if !patient.nil?
-            patient.patient_labs.create!(
-              date_of_test: remote_lab[:date_of_test],
-              lab_number: remote_lab[:lab_number],
-              clinic_code: remote_lab[:clinic_code],
-              lab_studies: remote_lab[:lab_studies]
-            )
-          end
-
-          # create_patient_lab(patient, remote_lab)
+          create_patient_lab(patient, remote_lab)
         end
       end
 
-    def self.create_patient(payload)
-      unless Patient.exists?(id_number: payload[:id_number] || payload[:patient_data][:id_number])
-        patient = Patient.create!(
-          id_number: payload[:id_number] || payload[:patient_data][:id_number],
-          first_name: payload[:patient_name] || payload[:patient_data][:first_name],
-          last_name: payload[:patient_name] || payload[:patient_data][:last_name] ,
-          date_of_birth: payload[:date_of_birth] || payload[:patient_data][:date_of_birth],
-          phone_mobile: "NA" || payload[:patient_data][:phone_mobile],
-          gender: payload[:gender] || payload[:patient_data][:gender]
-        )
-      end
-      patient
-    end
+      private
+        def self.create_patient(payload)
+          unless Patient.exists?(id_number: payload[:id_number] || payload[:patient_data][:id_number])
+            patient = Patient.create!(
+              id_number: payload[:id_number] || payload[:patient_data][:id_number],
+              first_name: payload[:patient_name] || payload[:patient_data][:first_name],
+              last_name: payload[:patient_name] || payload[:patient_data][:last_name] ,
+              date_of_birth: payload[:date_of_birth] || payload[:patient_data][:date_of_birth],
+              phone_mobile: "NA" || payload[:patient_data][:phone_mobile],
+              gender: payload[:gender] || payload[:patient_data][:gender]
+            )
+          end
+          patient
+        end
 
-    def self.create_patient_lab(patient, payload)
-        PatientLab.create!(
-          patient_id: patient,
-          date_of_test: payload[:date_of_test],
-          lab_number: payload[:lab_number],
-          clinic_code: payload[:clinic_code],
-          lab_studies: payload[:lab_studies]
-        )
-    end
+        def self.create_patient_lab(patient, payload)
+         if !patient.nil?
+            patient.patient_labs.create!(
+            date_of_test: payload[:date_of_test],
+            lab_number: payload[:lab_number],
+            clinic_code: payload[:clinic_code],
+            lab_studies: payload[:lab_studies]
+          )
+          end
+        end
   end
 end
